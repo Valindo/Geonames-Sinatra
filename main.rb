9ee8f0fec8821d1d4532f1973459e170eb6098f9
valindo <valindo.godinho@gmail.com>
2015-07-15 20:33:20 +0530

    Stable state

--- main.rb
+++ main.rb
@@ -1,6 +1,38 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 
 get '/' do
+	@a=10
 	erb :index
 end
+
+post '/search' do
+	data = params[:name]
+	data.gsub!(/\s/,"+")
+	uri = URI('http://api.geonames.org/searchJSON?q='+data+'&maxRows=10&username=valindo')
+	jsonfile = Net::HTTP.get(uri)
+	@my_hash = JSON.parse(jsonfile)
+	erb :search
+end
+
+
+get '/find/:name' do
+	data = params[:name]
+	uri = URI('http://api.geonames.org/searchJSON?name='+data+'&maxRows=10&username=valindo')
+	jsonfile = Net::HTTP.get(uri)
+	@my_hash = JSON.parse(jsonfile)
+	erb :search
+end
+
+get '/check_nearby/:lat/:lng/:fcode' do
+	lat = params[:lat]
+	lng = params[:lng]
+	fcode = params[:fcode]
+	uri = URI('http://api.geonames.org/findNearbyJSON?lng='+lng+'&lat='+lat+'&featureCode='+fcode+'&radius=100&username=valindo')
+	jsonfile = Net::HTTP.get(uri)
+	@my_hash = JSON.parse(jsonfile)
+	erb :search
+end
+
