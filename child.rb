d3758e4e5797b895f504c0c5abebbdec824748c0
valindo <valindo.godinho@gmail.com>
2015-07-20 23:09:23 +0530

    Trying to make DS, Child.rb

--- child.rb
+++ child.rb
@@ -1,6 +1,9 @@
 class Child
   attr_accessor :countryName, :lng, :fcodeName, :name, :fcode, :geonameId, :asciiName, :fcl
   attr_accessor :lat, :population, :countryId, :fclName, :countryCode, :toponymName, :continentCode
+  attr_accessor :continent, :capital, :languages, :south, :isoAlpha3, :north, :fipsCode, :population, :east
+  attr_accessor :isoNumeric, :areaInSqKm, :countryCode, :west, :continentName, :currencyCode
+
   def initialize(hash)
     hash.each_pair do |k, v|
       self.send("#{k}=",v) if self.respond_to?("#{k}=")
